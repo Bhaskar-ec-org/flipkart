ac0bb3ae8f0dd813f4800b47f770883766e32fbe
Bhaskar reddy <bhaskarreddy829@gmail.com>
2019-09-11 17:44:59 +0530

    updated file

--- Users.java
+++ Users.java
@@ -3,7 +3,10 @@ Hello guys
 to day iam going to git hub commands
 
 and kubernetes and jenkins
+<<<<<<< HEAD
 hello password less
 git clone
 
 git stash apply
+=======
+>>>>>>> d1e6e05... second commited
